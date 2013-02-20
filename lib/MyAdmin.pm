cb440a6b1867abce1f5b32d8b3978d366b1bee8b
tokuhirom <tokuhirom@gmail.com>
2013-02-21 05:19:49 +0900

    initial import

---
+++ lib/MyAdmin.pm
@@ -0,0 +1,41 @@
+package MyAdmin;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+MyAdmin - ...
+
+=head1 SYNOPSIS
+
+  use MyAdmin;
+
+=head1 DESCRIPTION
+
+MyAdmin is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
