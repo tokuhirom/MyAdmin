65272016cf66c4355108dd226d84ac2841e6a724
tokuhirom <tokuhirom@gmail.com>
2013-02-21 07:15:54 +0900

    show schema from db

--- lib/MyAdmin/MySQL.pm
+++ lib/MyAdmin/MySQL.pm
@@ -9,9 +9,35 @@ use DBI;
 
 sub dbh {
     my $c = shift;
-    DBI->connect(
+    $c->{dbh} ||= DBI->connect(
         @{$c->config->{database}}
     ) or die $DBI::errstr;
+}
+
+sub _validate {
+    my $stuff = shift;
+    $stuff =~ /\A[A-Za-z0-9_]+\z/ or die "Invalid name: $stuff";
+}
+
+sub table {
+    my $c = shift;
+    my $table = $c->req->param('table') // die;
+    _validate($table);
+    $table;
+}
+
+sub database {
+    my $c = shift;
+    my $database = $c->req->param('database') // die;
+    _validate($database);
+    $database;
+}
+
+sub use_db {
+    my $c = shift;
+
+    my $database = $c->database;
+    $c->dbh->do(qq{USE $database});
 }
 
 get '/' => sub {
@@ -30,23 +56,37 @@ get '/' => sub {
 get '/database' => sub {
     my $c = shift;
 
-    my $database = $c->req->param('database') // die;
-    $database =~ /\A[A-Za-z0-9_]+\z/ or die "Invalid database name: $database";
-
     my $dbh = $c->dbh;
-    $dbh->do(qq{USE $database});
+    $c->use_db();
     my @tables = map { @$_ } @{
-        $dbh->selectall_arrayref(q{SHOW TABLES});
+        $c->dbh->selectall_arrayref(q{SHOW TABLES});
     };
 
     $c->render('mysql/database.tt' => {
-        database => $database,
+        database => $c->database,
         tables => \@tables,
     });
 };
 
-get '/table' => sub {
+get '/list' => sub {
     ... # yes. it's not implemented yet.
+};
+
+get '/schema' => sub {
+    my $c = shift;
+
+    $c->use_db();
+
+    my $table = $c->table;
+    my ($schema) = map { $_->[1] } @{
+        $c->dbh->selectall_arrayref(qq{SHOW CREATE TABLE $table});
+    };
+
+    $c->render('mysql/schema.tt' => {
+        database => scalar($c->req->param('database')),
+        table => $table,
+        schema => $schema,
+    });
 };
 
 
