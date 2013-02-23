6b821728cf5c802c6db91cc660095ee0b75e8f15
tokuhirom <tokuhirom@gmail.com>
2013-02-23 23:15:13 +0900

    better exception handling

--- lib/MyAdmin/MySQL.pm
+++ lib/MyAdmin/MySQL.pm
@@ -38,9 +38,13 @@ sub dbh {
         my @config = @{$c->config->{database}};
         $config[3]->{mysql_enable_utf8} //= 1;
         $config[3]->{ShowErrorStatement} //= 1;
+        $config[3]->{RaiseError} = 1;
         my $dbh = DBI->connect(
             @config
         ) or MyAdmin::Exception->throw($DBI::errstr);
+        $dbh->{HandleError} = sub {
+            MyAdmin::Exception->throw($_[0])
+        };
         $dbh->do(q{SET SESSION sql_mode=STRICT_TRANS_TABLES;});
         $dbh;
     };
